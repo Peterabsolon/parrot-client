ca73b6b429c6ea2d6d3d7c6bfe4e4e14ef38bb56
Peter <peterabsolon@yahoo.com>
2020-10-24 23:01:55 +0200

    Fix stuff

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,5 +1,5 @@
-export const APP_NAME = 'Amazing boilerplate'
-export const APP_DESCRIPTION = 'TS, Next, MobX'
+export const APP_NAME = 'Parrot'
+export const APP_DESCRIPTION = 'Tokenizer'
 
 export const API_URL = process.env.API_URL || 'https://cat-fact.herokuapp.com'
 export const GRAPHQL_URL = process.env.GRAPHQL_URL || 'https://graphql-weather-api.herokuapp.com/'
